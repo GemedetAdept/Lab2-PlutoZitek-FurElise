f0fed2b5624bbdb415785c6d0cdf96a0d2d70723
Vivian Zitek <vivian.zitek@gmail.com>
2022-12-05 21:35:11 -0700

    Test out A4-C5-E5 minor triad.

--- Program.cs
+++ Program.cs
@@ -5,21 +5,30 @@ var sineOne = new SignalGenerator() {
 	Gain = 0.2, 
 	Frequency = 220,
 	Type = SignalGeneratorType.Sin}
-	.Take(TimeSpan.FromSeconds(1));
+	.Take(TimeSpan.FromSeconds(2));
 
 var sineTwo = new SignalGenerator() { 
 	Gain = 0.2, 
 	Frequency = 330,
 	Type = SignalGeneratorType.Sin}
-	.Take(TimeSpan.FromSeconds(1));
+	.Take(TimeSpan.FromSeconds(2));
+
+var sineThree = new SignalGenerator() { 
+	Gain = 0.2, 
+	Frequency = 262,
+	Type = SignalGeneratorType.Sin}
+	.Take(TimeSpan.FromSeconds(2));
 
 using (var woA = new WaveOutEvent())
 using (var woB = new WaveOutEvent())
+using (var woC = new WaveOutEvent())
 {
 	woA.Init(sineOne);
 	woB.Init(sineTwo);
+	woC.Init(sineThree);
 	woA.Play();
 	woB.Play();
+	woC.Play();
 	while (woA.PlaybackState == PlaybackState.Playing && woB.PlaybackState == PlaybackState.Playing) {
 		Thread.Sleep(500);
 	}
