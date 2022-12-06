55a9ffe374bb56f540b3f725a6b697c7776109ed
Vivian Zitek <vivian.zitek@gmail.com>
2022-12-05 21:18:41 -0700

    Testing out NAudio as a way of playing simultaneous notes and chords.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+var sineOne = new SignalGenerator() { 
+	Gain = 0.2, 
+	Frequency = 220,
+	Type = SignalGeneratorType.Sin}
+	.Take(TimeSpan.FromSeconds(1));
+
+var sineTwo = new SignalGenerator() { 
+	Gain = 0.2, 
+	Frequency = 330,
+	Type = SignalGeneratorType.Sin}
+	.Take(TimeSpan.FromSeconds(1));
+
+using (var woA = new WaveOutEvent())
+using (var woB = new WaveOutEvent())
+{
+	woA.Init(sineOne);
+	woB.Init(sineTwo);
+	woA.Play();
+	woB.Play();
+	while (woA.PlaybackState == PlaybackState.Playing && woB.PlaybackState == PlaybackState.Playing) {
+		Thread.Sleep(500);
+	}
+}
