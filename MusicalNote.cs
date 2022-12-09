ac3248cd818a1c57444e0014ac5ed8e0251b4a63
Vivian Zitek <vivian.zitek@gmail.com>
2022-12-09 05:28:02 -0700

    Remove Tone.cs redundancy. Implement audio player via MusicalNote objects.

--- MusicalNote.cs
+++ MusicalNote.cs
@@ -1,8 +1,69 @@
 public class MusicalNote {
 
-	public MusicalNote() {
+	private int _duration;
+	private string _noteLetter;
+	private int _octave;
+	private double _frequency;
+	const int RootFrequency = 55;
+	const int OctaveMin = 1;
+	const int OctaveMax = 8;
+	protected Dictionary<string, int> NoteValues = new Dictionary<string, int> {
+		{"C~",-9},
+		{"C#",-8},
+		{"Db",-8},
+		{"D~",-7},
+		{"D#",-6},
+		{"Eb",-6},
+		{"E~",-5},
+		{"F~",-4},
+		{"F#",-3},
+		{"Gb",-3},
+		{"G~",-2},
+		{"G#",-1},
+		{"Ab",-1},
+		{"A~",0},
+		{"A#",1},
+		{"Bb",1},
+		{"B~",2},
+	};
 
+	public MusicalNote(int duration, string note, int octave) {
 
+		Duration = duration;
+		NoteLetter = note;
+		Octave = octave;
+
+		int halfSteps = NoteValues[NoteLetter];
+		double twelthRootofTwo = Math.Pow(2, (1.0/12.0));
+		Frequency = (RootFrequency*(Math.Pow(twelthRootofTwo, halfSteps)))*(Octave*2);
+	}
+
+	public int Duration {
+
+		get {return _duration;}
+		set {_duration = value;}
+	}
+	public string NoteLetter {
+
+		get {return _noteLetter;}
+		set {
+			string[] notes = NoteValues.Keys.ToArray();
+			if (notes.Contains(value)) {_noteLetter = value;}
+			else {Console.WriteLine($"{value} is an invalid note.");}
+		}
+	}
+	public int Octave {
+
+		get {return _octave;}
+		set {
+			if (value >= OctaveMin && value <= OctaveMax) {_octave = value;}
+			else {Console.WriteLine($"Octave {value} is out of the range [{OctaveMin} - {OctaveMax}].");}
+		}
+	}
+	public double Frequency {
+
+		get {return _frequency;}
+		set {_frequency = value;}
 	}
 
 
--- Program.cs
+++ Program.cs
@@ -1,3 +1,49 @@
-﻿Tone newTone = new Tone();
-newTone.NoteLetter = "R";
-newTone.Octave = 9;
+﻿using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+List<int> noteDurations = new List<int>()
+	{2, 2, 2};
+List<string> noteNames = new List<string>()
+	{"A~", "E~", "C~"};
+List<int> noteOctaves = new List<int>()
+	{3, 4, 4};
+
+List<MusicalNote> notesList = new List<MusicalNote>();
+for (int i=0; i < noteDurations.Count; i++) {
+	MusicalNote newNote = new MusicalNote(
+		noteDurations[i],
+		noteNames[i],
+		noteOctaves[i]
+	);
+
+	Console.WriteLine($"{newNote.NoteLetter}{newNote.Octave}, {newNote.Frequency}");
+
+	notesList.Add(newNote);
+}
+
+var noteSignals = notesList.Select( x =>
+	new SignalGenerator() { 
+		Gain = 0.2, 
+		Frequency = x.Frequency, // These are doubles!!!
+		Type = SignalGeneratorType.Sin}
+		.Take(TimeSpan.FromSeconds(x.Duration))
+).ToList();
+
+void playNotes() {
+	using (var woA = new WaveOutEvent())
+	using (var woB = new WaveOutEvent())
+	using (var woC = new WaveOutEvent())
+	{
+		woA.Init(noteSignals[0]);
+		woB.Init(noteSignals[1]);
+		woC.Init(noteSignals[2]);
+		woA.Play();
+		woB.Play();
+		woC.Play();
+		while (woA.PlaybackState == PlaybackState.Playing && woB.PlaybackState == PlaybackState.Playing) {
+			Thread.Sleep(500);
+		}
+	}
+}
+
+playNotes();
